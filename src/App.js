f1b5fa8da609b925a76e640f3d5472aba077776e
origogi <rlawjdxo1212@naver.com>
2021-12-28 17:49:02 +0900

    add Home.js, Home.css

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
 import './App.css';
+import Home from './Home';
 
 function App() {
   return (
     <div className='App'>
       <h1>Let's build AIRBNB clone</h1>
+
+      <Home />
+
+      {/* Home */}
+        {/* Header */}
+        {/* Banner */}
+          {/* Search */}
+        {/* Card */}
+        {/* Footer */}
+
+      {/* Search Page */}
     </div>
   );
 }

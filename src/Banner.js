198dfac2d6e840510fe40b9e338146df36639ec0
origogi <rlawjdxo1212@naver.com>
2021-12-30 15:53:23 +0900

    update showSearch button

--- src/Banner.js
+++ src/Banner.js
@@ -1,12 +1,29 @@
-import React from 'react'
-import './Banner.css'
+import { Button } from "@mui/material";
+import React, { useState } from "react";
+import "./Banner.css";
 
 function Banner() {
-    return (
-        <div>
-            
-        </div>
-    )
+  const [showSearch, setShowSearch] = useState(false);
+
+  return (
+    <div className="banner">
+      <div className="banner__search">
+        {showSearch && <h1>SHOW DATE PICKER</h1>}
+        <Button variant="outlined" className="banner__searchButton" onClick= {
+          () => setShowSearch(!showSearch)
+        }>
+          Search Dates
+        </Button>
+      </div>
+      <div className="banner__info">
+        <h1>Get out and stretch your imagination</h1>
+        <h5>
+          Plan an different kind of gateway to uncover the hidden gems near you.
+        </h5>
+        <Button variant="outlined">Explore Nearby</Button>
+      </div>
+    </div>
+  );
 }
 
-export default Banner
+export default Banner;

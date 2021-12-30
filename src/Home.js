e404ad89d2f8faf6ac23799a51627b7296deb302
origogi <rlawjdxo1212@naver.com>
2021-12-30 17:56:27 +0900

    Add dummy data

--- src/Home.js
+++ src/Home.js
@@ -1,22 +1,45 @@
 import React from "react";
 import "./Home.css";
 import Banner from "./Banner";
+import Card from "./Card";
 
 function Home() {
   return (
     <div className="home">
       <Banner />
       <div className="home__section">
-        <Card />
-        <Card />
-        <Card />
-
+        <Card
+          src="https://www.traveldailymedia.com/assets/2021/11/airbnb-1-min.jpeg"
+          title="Entire"
+          description="Comfortable private places, with room for friends or family."
+        />
+        <Card
+          src="https://www.traveldailymedia.com/assets/2021/11/airbnb-1-min.jpeg"
+          title="Entire"
+          description="Comfortable private places, with room for friends or family."
+        />
+        <Card
+          src="https://www.traveldailymedia.com/assets/2021/11/airbnb-1-min.jpeg"
+          title="Entire"
+          description="Comfortable private places, with room for friends or family."
+        />
       </div>
       <div className="home__section">
-        <Card />
-        <Card />
-        <Card />
-
+        <Card
+          src="https://www.traveldailymedia.com/assets/2021/11/airbnb-1-min.jpeg"
+          title="Entire"
+          description="Comfortable private places, with room for friends or family."
+        />
+        <Card
+          src="https://www.traveldailymedia.com/assets/2021/11/airbnb-1-min.jpeg"
+          title="Entire"
+          description="Comfortable private places, with room for friends or family."
+        />
+        <Card
+          src="https://www.traveldailymedia.com/assets/2021/11/airbnb-1-min.jpeg"
+          title="Entire"
+          description="Comfortable private places, with room for friends or family."
+        />
       </div>
     </div>
   );

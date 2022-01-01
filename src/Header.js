ecf5f95baa6ab2928c5bbab002814bfaa0207c44
origogi <rlawjdxo1212@naver.com>
2022-01-01 21:30:14 +0900

    add home page router link

--- src/Header.js
+++ src/Header.js
@@ -2,14 +2,18 @@ import { ExpandMore, Language, Search } from "@mui/icons-material";
 import React from "react";
 import "./Header.css";
 import { Avatar } from "@mui/material";
+import { Link } from "react-router-dom";
 
 function Header() {
   return (
     <div className="header">
-      <img
-        className="header__icon"
-        src="https://i.pinimg.com/564x/c6/49/8b/c6498bc6f8fe8336b8aa1b5232cc36e8.jpg"
-      />
+      <Link to="/">
+        <img
+          className="header__icon"
+          src="https://i.pinimg.com/564x/c6/49/8b/c6498bc6f8fe8336b8aa1b5232cc36e8.jpg"
+        />
+      </Link>
+
       <div className="header__center">
         <input type="text" />
         <Search />

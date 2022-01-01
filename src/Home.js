334950903f8f0a7aa44763395341e04e61e93b31
origogi <rlawjdxo1212@naver.com>
2022-01-01 22:09:06 +0900

    fix dummy data

--- src/Home.js
+++ src/Home.js
@@ -9,39 +9,39 @@ function Home() {
       <Banner />
       <div className="home__section">
         <Card
-          src="https://www.traveldailymedia.com/assets/2021/11/airbnb-1-min.jpeg"
-          title="Entire"
-          description="Comfortable private places, with room for friends or family."
+          src="https://a0.muscache.com/im/pictures/eb9c7c6a-ee33-414a-b1ba-14e8860d59b3.jpg?im_w=720"
+          title="Online Experiences"
+          description="Unique activities we can do together, led by a world of hosts."
         />
         <Card
-          src="https://www.traveldailymedia.com/assets/2021/11/airbnb-1-min.jpeg"
-          title="Entire"
-          description="Comfortable private places, with room for friends or family."
+          src="https://a0.muscache.com/im/pictures/15159c9c-9cf1-400e-b809-4e13f286fa38.jpg?im_w=720"
+          title="Unique stays"
+          description="Spaces that are more than just a place to sleep."
         />
         <Card
-          src="https://www.traveldailymedia.com/assets/2021/11/airbnb-1-min.jpeg"
-          title="Entire"
+          src="https://a0.muscache.com/im/pictures/fdb46962-10c1-45fc-a228-d0b055411448.jpg?im_w=720"
+          title="Entire homes"
           description="Comfortable private places, with room for friends or family."
         />
       </div>
       <div className="home__section">
         <Card
-          src="https://www.traveldailymedia.com/assets/2021/11/airbnb-1-min.jpeg"
-          title="Entire"
-          price="$29.99/night"
-          description="Comfortable private places, with room for friends or family."
+          src="https://media.nomadicmatt.com/2019/airbnb_breakup3.jpg"
+          title="3 Bedroom Flat in Bournemouth"
+          description="Superhost with a stunning view of the beachside in Sunny Bournemouth"
+          price="£130/night"
         />
         <Card
-          src="https://www.traveldailymedia.com/assets/2021/11/airbnb-1-min.jpeg"
-          title="Entire"
-          price="$29.99/night"
-          description="Comfortable private places, with room for friends or family."
+          src="https://thespaces.com/wp-content/uploads/2017/08/Courtesy-of-Airbnb.jpg"
+          title="Penthouse in London"
+          description="Enjoy the amazing sights of London with this stunning penthouse"
+          price="£350/night"
         />
         <Card
-          src="https://www.traveldailymedia.com/assets/2021/11/airbnb-1-min.jpeg"
-          title="Entire"
-          price="$29.99/night"
-          description="Comfortable private places, with room for friends or family."
+          src="https://media.nomadicmatt.com/2018/apartment.jpg"
+          title="1 Bedroom apartment"
+          description="Superhost with great amenities and a fabolous shopping complex nearby"
+          price="£70/night"
         />
       </div>
     </div>
